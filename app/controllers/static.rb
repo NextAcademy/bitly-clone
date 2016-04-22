9ea3b3903e49072a6c890a7a32710edc10cf1609
JLikesZzz <jeremy_lzz@hotmail.com>
2016-04-22 09:55:23 +0800

    more changes

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,23 +1,34 @@
 
 
 get '/' do
+  @uri = Url.all.sort_by {|x| x[:created_at]}.reverse
   erb :"static/index"
+
 end
 
 post '/urls' do
-  url1 = Url.shorten
-  @url = Url.new(long: params[:long_url], short: url1)
-  if @url.save == true
-  	@url
-  erb :"static/index" 
-  else
-  	redirect '/error'
-  end
-end
 
-get '/error' do
-	@url
-	erb :"static/index2"
+url1 = Url.shorten
+
+	text = params[:long_url]
+	regex = (/\(?(?:(http|https):\/\/)/)
+  	
+  if text.match(regex)
+  		params[:long_url] 
+  	else
+  		params[:long_url] = "http://" + params[:long_url]
+  end
+  @uri = Url.all.sort_by {|x| x[:created_at]}.reverse
+  	@url = Url.new(long: params[:long_url], short: url1)
+  	if @url.save == true
+  		 @url
+  	erb :"static/index" 
+  	else
+  		@url=nil
+  		@urll = true
+  	erb :"static/index"
+  	end
+
 end
 
 get '/list' do
--- app/models/url.rb
+++ app/models/url.rb
@@ -3,8 +3,8 @@
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 
-validates :long, :format => {:with => /(((ftp|http|https):\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/}
-
+validates :long, :format => {:with => /\(?(?:(http|https):\/\/)(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/}
+validates :short, :uniqueness => true
 	def self.shorten
 		letters_array = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U W X Y Z 1 2 3 4 5 6 7 8 9 0)
 		random_selection = letters_array.sample(6)
