9f94d48fa04ad613f7f7796aa1c8d52b432d7b4c
Aiman <imbacakes@gmail.com>
2016-12-08 09:58:58 +0800

    added sessions to the static.rb

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,17 +1,22 @@
+enable :sessions
+
 get '/' do
   @data = Url.all.last(5).reverse
+  @valid_url = session[:good_message]
+  @invalid_url = session[:bad_message]
   erb :"static/index"
 end
 
 post '/shorten_url' do
   url = Url.new(params)
   if url.save
-    @new_url = "Your new url is " + "http://localhost:9393/" + url.short_url
+    session[:good_message] = "Your new url is " + "http://localhost:9393/" + url.short_url
     redirect '/'
   else
-    @new_url = "Not a valid URL"
+    session[:bad_message] = "Not a valid URL"
     redirect '/'
   end
+end
 
   get '/:short_url' do
     object = Url.where(short_url: params[:short_url])
