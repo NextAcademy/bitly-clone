2a38a5437138de711edd45ed988e002b10b19eb9
user <likhimsiow@yahoo.com>
2016-12-08 17:00:40 +0800

    Added Click Count

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -5,22 +5,29 @@ end
 # #2 save long and short urls into database
 post '/url' do
 	#create new row of data with long url
-	#AR callback will auto run shorter=n
-
+	#AR callback will auto run shorten
 	url = Url.new(params)
 	url.save
 	redirect to("/")
   #OR erb :"static/index" #render
 end
 
-#3 redirect to actual page
+# #3a redirect to actual page
+# get '/:short_url' do
+# 	#find row of short url associated to long url
+# 	long_url = Url.find_by(short_url: params['short_url']).long_url
+# 	redirect to long_url
+# end
+
+
+#3b redirect to actual page & add counter
 get '/:short_url' do
 	#find row of short url associated to long url
-	long_url = Url.find_by(short_url: params['short_url']).long_url
-	redirect to long_url
+	url = Url.find_by(short_url: params['short_url'])
+	url.click_count += 1
+	url.save
+	redirect to url.long_url
 end
-
-
 
 
 
