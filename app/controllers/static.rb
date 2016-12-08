e112131beb4539b8fcfef8de002c69a529a635ae
user <likhimsiow@yahoo.com>
2016-12-08 14:30:48 +0800

    Static, index, url done

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,26 @@
 get '/' do
-  erb :"static/index"
+	erb :"static/index"
 end
+
+# #2 save long and short urls into database
+post '/url' do
+	#create new row of data with long url
+	#ARcallback willauto run shortern
+	url = Url.new(params)
+	url.save
+	redirect to("/")
+  #OR erb :"static/index" #render
+end
+
+#3 redirect to actual page
+get '/:short_url' do
+	#find row of short url associated to long url
+	long_url = Url.find_by(short_url: params['short_url']).long_url
+	redirect to long_url
+end
+
+
+
+
+
+
